efe44632e33414a39defa8aecec9eec7fe2e39ca
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-08 17:45:08 +0530

    anagram.js

--- easy/anagram.js
+++ easy/anagram.js
@@ -4,10 +4,34 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 function isAnagram(str1, str2) {
-  
+  const clearstr1 = str1.replace(/\s/g, '').toLowerCase();
+  const clearstr2 = str2.replace(/\s/g, '').toLowerCase();
+  if(clearstr1.length =! clearstr2.length){
+    return false;
+
+  }
+  function Frequency(str){
+    const Frequencymap = {}
+    for(const char of str){
+      Frequencymap[char] = (Frequencymap || 0 ) +1;
+    }
+    return Frequencymap;
+  }
+  function compare(map1,map2){
+    for(const key in map1){
+      if(map1[key] !== map2[key]){
+        return false;
+      }
+    }
+  }
+  return true;
   }
   
   module.exports = isAnagram;
+  const str1 = 'listen';
+  const str2 = 'silent';
+  const result =isAnagram(str1,str2)
+  console.log(result);
   
   
   
