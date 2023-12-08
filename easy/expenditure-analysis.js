dcaf861a755e4af570644c1b7dc528422da8392c
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-08 16:02:53 +0530

    2nd task complete

--- easy/expenditure-analysis.js
+++ easy/expenditure-analysis.js
@@ -5,8 +5,34 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  const totalCategory = {}
+  transactions.forEach(transaction => {
+    const{category,price} = transaction
+  
+  // if there is no category present then 
+  if(!totalCategory[category]){
+    totalCategory[category] = price
+  }
+  else{
+    totalCategory[category] =+ price
+  }
+});
+  const result = Object.keys(totalCategory).map(category =>({
+    [category] :totalCategory[category]
+  }))
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
+
+// let's take an example
+const price = [
+{itemname:'item1',category:'sugar',price:'2000',timestamp:'12-2-23'},
+{itemname:'item2',category:'salt',price:'3000',timestamp:'21-2-23'},
+{itemname:'item3',category:'oil',price:'4000',timestamp:'02-2-23'}
+]
+const totalPrice = calculateTotalSpentByCategory(price)
+console.log("total price is:",totalPrice);
